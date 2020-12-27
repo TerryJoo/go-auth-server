4abbedafa1b1276f60cd55fc67ecaf76ade358b0
Terry <yungik@glocalizeinc.com>
2020-12-27 18:17:13 +0900

    Add GORM
    Squashed commit of the following:
    
    commit f6f6152f56a9895635cc7d19399c6e65706c7964
    Author: Terry <yungik@glocalizeinc.com>
    Date:   Sun Dec 27 18:14:35 2020 +0900
    
        Inject DB
    
    commit 1ae59d58486c4581880e3395056bfe957f1ac4c7
    Author: Terry <yungik@glocalizeinc.com>
    Date:   Sun Dec 27 18:12:07 2020 +0900
    
        Update gitignore
    
    commit 4f546f6b367c0b72eed0094d91d39ca8a192c904
    Author: Terry <yungik@glocalizeinc.com>
    Date:   Sun Dec 27 18:09:17 2020 +0900
    
        Add middleware
    
    commit 30d7fd2faf5436d80a1e12ed44f24b59af18fcd2
    Author: Terry <yungik@glocalizeinc.com>
    Date:   Sun Dec 27 18:08:11 2020 +0900
    
        Add DB initialization logic & DB configs
    
    commit ce2a91b0faacc00fd019d026453b84fc3da74cc4
    Author: Terry <yungik@glocalizeinc.com>
    Date:   Sun Dec 27 18:07:07 2020 +0900
    
        Add dependencies

--- configs.go
+++ configs.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"crypto"
+	"fmt"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 const (
@@ -10,3 +13,20 @@ const (
 	BCryptCost = bcrypt.DefaultCost
 	SecretKey  = "SECRET_KEY" // TOOD: Must change this value
 )
+
+// Database
+const (
+	DSN      = "develop.db"
+	LogLevel = 4
+)
+
+func InitDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(DSN), &gorm.Config{})
+	if err != nil {
+		println("Failed to connect database. Reason: ", err.Error())
+		panic("Failed to connect database")
+	}
+	fmt.Println("Connected to database")
+	db.Logger.LogMode(LogLevel)
+	return db
+}
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jooyungik/auth-server/middlewares"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(middlewares.InjectDB(InitDB()))
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
